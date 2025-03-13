bb0a932463af788449105d8c05a43096ce4bd3c2
tnprf <tauanitalo10@gmail.com>
2025-03-13 20:41:44 -0300

    Create 07.py

---
+++ 07.py
@@ -0,0 +1,14 @@
+#7  Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.
+
+
+print("       *")
+print("      XXX ")
+print("     XXX*X")
+print("    X&XXX$X")
+print("   X%XXXX@XX")
+print("  XXX#XXX!XXX")
+print(" XX+&XXXXXX!XX")
+print("XXXXXXXXXXXXXXX")
+print("      XX")
+print("      XX")
+print("     XXXXX")
