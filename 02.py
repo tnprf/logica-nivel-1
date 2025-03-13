0642ccc0d5055dca3df831a4be747863860fafe7
tnprf <tauanitalo10@gmail.com>
2025-03-13 19:38:16 -0300

    Create 02.py

---
+++ 02.py
@@ -0,0 +1,4 @@
+#2 Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira
+print("tauan italo")
+print("rio de janeiro")
+print("rio de janeiro")
