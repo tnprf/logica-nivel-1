3f48759fd644eaab203171e03e5653cf0fe7b735
tnprf <tauanitalo10@gmail.com>
2025-03-13 20:32:33 -0300

    Create 06.py

---
+++ 06.py
@@ -0,0 +1,10 @@
+#6  Elabore um programa que mostre o seguinte menu na tela:
+
+print("CADASTRO DE CLIENTES")
+print("0 - Fim")
+print("1 - Inclui")
+print("2 - Altera")
+print("3 - Exclui")
+print("4 - Consulta")
+
+print("Opção")
